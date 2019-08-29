ed13011f282634f3c26ecf953d1b6a48e99fbc83
Diptis Halder <diptis.halder@gmail.com>
2019-08-29 17:03:38 +0100

    :white_check_mark: checking for first run of fn with didrun property - tests are failing but useful error messages

--- rectIntoSqr.js
+++ rectIntoSqr.js
@@ -1,13 +1,19 @@
 function sqInRect(lng, wdth) {
-  if (lng == wdth) {
-    return lng;
+  if (!sqInRect.didrun) {
+    if (lng == wdth)  {
+      sqInRect.didrun = true;
+      return null;
+    }
+    else if (lng == wdth)  {
+      return lng;
+    }
+    if (lng < wdth) {
+      temp = lng;
+      lng = wdth;
+      wdth = temp;
+    }
+    return [wdth].concat(sqInRect(wdth, lng-wdth));
   }
-  if (lng < wdth) {
-    temp = lng;
-    lng = wdth;
-    wdth = temp;
-  }
-  return [wdth].concat(sqInRect(wdth, lng-wdth));
 }
 
 module.exports = sqInRect;
